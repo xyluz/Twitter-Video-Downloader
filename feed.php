64b3e49c272ca239ad70ae9f22aa9c3bcf98f4aa
Seyi Onifade <xyluz@ymail.com>
2017-03-28 20:48:19 +0100

    touches

--- feed.php
+++ feed.php
@@ -1,26 +1,29 @@
 <?php
 include_once('GetTwitterFeed.class.php');
 
-
 $retrieveUrl = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=xyluz";
 $consumer_key = "12u86gn9yvvkcYFRm3lsWcYz7";
 $consumer_key_secret = "iKjf5T621LAkVk65NX3Jt9TTlJifSnnccpdEZd9HolscYXvEBF";
+
 $objTwitter = new GetTwitterFeed($retrieveUrl, $consumer_key, $consumer_key_secret);
 
- $raw_feed = json_decode($objTwitter->getJsonFeed(),TRUE);
-$counter = 0;
-foreach($raw_feed as $feed){
+$raw_feed = json_decode($objTwitter->getJsonFeed(),TRUE);
 
-$strip_feed = $feed[$counter]['extended_entities']['media'][0]['video_info']['variants'];
-
-//loop
-//print_r($feed[0][extended_entities][media][0][video_info][variants]);
+$count_feed = count($raw_feed);
 
 echo "<!html><head><title>Twitter Video Downloader</title></head><body>";
 
+for($counter = 0; $counter <= $count_feed; $counter++){
+
+$strip_feed = $raw_feed[$counter]['extended_entities']['media'][0]['video_info']['variants'];
+
+//loop
+//print_r($strip_feed);
+
+
 foreach($strip_feed as $strip){
 
-echo " <video width='320' height='240' controls>
+echo "<video width='320' height='240' controls>
   <source src='" . $strip['url'] . "' type='" . $strip['content_type'] . "'>
  
 Your browser does not support the video tag.
@@ -30,7 +33,7 @@ echo "<a href='" . $strip['url'] ."'>" . $strip['content_type']."</a>";
 
 }
 
-echo "</body></html>";
 }
 
+echo "</body></html>";
 ?>
